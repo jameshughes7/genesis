51320060417cb178ae1cfa30ceaef2b1ac26b07a
jameshughes7 <hugjimbo77@gmail.com>
2019-04-10 12:18:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
+    // JSX must have one root element
+    // So various elements need to be included within the div
+    // e.g. both h1 elements here
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Hi, I'm a React App</h1>
+      <h1>Another heading</h1>
+      <Person name="James" age="36" />
+      <Person name="Caryn" age="39" />
+      <Person name="Gabriel" age="2" >My Hobbies: BibleMan, Superheroes!</Person>
       </div>
     );
+    
+    // If you don't use JSX you would need to create react elements as so:
+    // createElement takes at least 3 args
+    // 1. The element we want to render to the DOM
+    // 2. configuration for this
+    // 3. Any amount of children (i.e. what's nested inside div)
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
