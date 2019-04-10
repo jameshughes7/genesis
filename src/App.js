186a9930a99862219fc8d74589d943bd75e54005
jameshughes7 <hugjimbo77@gmail.com>
2019-04-10 15:51:19 +0100

    passing method references between components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// App is a stateful component as it manages state
+// Also known as smart components or container components
 class App extends Component {
   state = {
     persons: [
@@ -12,17 +14,20 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!')
     // DON'T DO THIS!!! MUTATE STATE DIRECTLY!!! this.state.persons[0].name = "James Andrew Hughes"
     this.setState({
       persons: [
-        { name: 'James Andrew Hughes', age: 36 },
+        { name: newName, age: 36 },
         { name: 'Caryn', age: 27 },
         { name: 'Gabriel', age: 2 }
       ]
     })
   }
+
+  // Note 2 different syntax examples for passing arguments
+  // 2 e.g.'s on line 40 and on line 52
 
   render() {
     // JSX must have one root element
@@ -32,12 +37,27 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <h1>Another heading</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >My Hobbies: BibleMan, Superheroes!</Person>
+        <button onClick={() => this.switchNameHandler('Reginald')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, 'Tennessee')} >My Hobbies: BibleMan, Superheroes!
+        </Person>
       </div>
     );
+    
+    // Can pass methods as props as you can see above with switchNameHandler
+    // being passed to click property
+    // Can pass click handlers from child components to Parent components
+    // as child component doesn't have access to the state
     
     // If you don't use JSX you would need to create react elements as so:
     // createElement takes at least 3 args
